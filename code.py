0d54f1a8008e225cdde530646c12877b5d92ae48
Chitransh <chitranshgaur@outlook.com>
2025-01-05 17:17:51 +0530

    initial commit

--- code.py
+++ code.py
@@ -1,3 +1,3 @@
-print("chitransh")
-i just want to check if the code is getting updated or not
-jj
+# print("chitransh")
+# i just want to check if the code is getting updated or not
+# i think now i know how to push and commit changes on gitbash
